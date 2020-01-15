5e93682b1860e92066d549ed571ffcfe7aeaf676
yudachao <290945266@qq.com>
2020-01-16 00:45:39 +0800

    feat: 🎸 初始化项目，添加vue

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">{{ text }}</div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        text: 'vue srr'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    color: blueviolet;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024, // 小于1024字节 编译成base64
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
